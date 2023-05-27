f87636acc869e70620c15af8a2b4a205ef857dcd
VUK <vadamovic7621rn@raf.rs>
2023-05-27 14:07:38 +0200

    Homo page vizuelno uradjen

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
   <div id="app">
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    <LoginPage/>
+    <!-- <LoginPage/> -->
+    <HomePage/>
   </div>
 </template>
 
 <script>
-import LoginPage from './components/LoginPage.vue'
+// import LoginPage from './components/LoginPage.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    LoginPage
+    // LoginPage,
+    HomePage
   }
 }
 </script>
 
 <style>
-#app {
+/* #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
+} */
 </style>
