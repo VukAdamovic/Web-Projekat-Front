4bb522842bde4913d14813a9fd05d22db287b878
VUK <vadamovic7621rn@raf.rs>
2023-05-29 20:41:50 +0200

    Users page + forma

--- src/App.vue
+++ src/App.vue
@@ -3,33 +3,38 @@
     <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <!-- <LoginPage/> -->
     <!-- <HomePage/> -->
-    <SingleNewsPage/>
+    <!-- <SingleNewsPage/> -->
     <!-- <NewsPage/> -->
     <!-- <CreateNewsPage/> -->
     <!-- <CategoriesPage/> -->
     <!-- <CreateCategoryPage/> -->
+    <!-- <UsersPage/> -->
+    <CreateUserPage/>
   </div>
 </template>
 
 <script>
 // import LoginPage from './components/LoginPage.vue'
 // import HomePage from './components/HomePage.vue'
-import SingleNewsPage from './components/news/SingleNewsPage.vue'
+// import SingleNewsPage from './components/news/SingleNewsPage.vue'
 // import NewsPage from './components/news/NewsPage.vue'
 // import CreateNewsPage from './components/news/CreateNewsPage.vue'
-// import CategoriesPage from './components/category/CategoriesPage.vue';
-// import CreateCategoryPage from './components/category/CreateCategoryPage.vue';
-
+// import CategoriesPage from './components/category/CategoriesPage.vue'
+// import CreateCategoryPage from './components/category/CreateCategoryPage.vue'
+// import UsersPage from './components/user/UsersPage.vue'
+  import CreateUserPage from './components/user/CreateUserPage.vue'
 export default {
   name: 'App',
   components: {
     // LoginPage,
     // HomePage
-    SingleNewsPage
+    // SingleNewsPage
     // NewsPage
     // CreateNewsPage
     // CategoriesPage
     // CreateCategoryPage
+    // UsersPage
+    CreateUserPage
 }
 }
 </script>
---
+++ src/components/user/CreateUserPage.vue
@@ -0,0 +1,249 @@
+<template>
+  <div id="createUserPage" style="height: 100vh;">
+  
+      <nav class="navbar navbar-expand-lg navbar-light border-navbar m-0">
+  
+          <div class="container-fluid justify-content-center">
+  
+  
+  
+              <a class="navbar-brand ps-5  text-navbar">
+  
+                  <img src="../../assets/logo-raf.png" alt="Logo" width="60" height="60" class="d-inline-block">
+  
+                      RAF News
+  
+              </a>
+  
+  
+  
+              <button class="navbar-toggler custom-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  
+                  <span class="navbar-toggler-icon"></span>
+  
+              </button>
+  
+  
+  
+              <div class="collapse navbar-collapse justify-content-end  padding-left-right-125px" id="navbarNav">
+  
+                  <ul class="navbar-nav">
+  
+                      <li class="nav-item text-center m-3">
+  
+                          <a class="nav-link  text-navbar" href="#">Home</a>
+  
+                      </li>
+  
+                      <li class="nav-item text-center m-3">
+  
+                          <a class="nav-link text-navbar" href="#">Top Stories</a>
+  
+                      </li>
+  
+                      <li class="nav-item text-center m-3">
+  
+                          <a class="nav-link text-navbar" href="#">Categories</a>
+  
+                      </li>
+
+                      <li class="nav-item text-center m-3">
+
+                          <a class="nav-link text-navbar" href="#">News</a>
+
+                      </li>
+  
+                      <li class="nav-item text-center m-3">
+  
+                          <a class="nav-link text-navbar" href="#">Users</a>
+  
+                      </li>
+  
+                      <li class="nav-item text-center m-3">
+  
+                          <a class="nav-link text-navbar" href="#">Log out</a>
+  
+                      </li>
+  
+                  </ul>
+  
+              </div>
+  
+          </div>
+  
+      </nav>
+  
+      <div class="container mt-2">
+  
+          <div class="container-fluid p-5 row justify-content-center">
+  
+              <form class="p-5 col glass card-form text-center custom-bar">
+  
+                  <h4>New User</h4>
+
+                  <div class="input-group mt-4 mb-3">
+                    <input type="text" aria-label="First name" class="form-control" placeholder="First Name">
+                    <input type="text" aria-label="Last name" class="form-control" placeholder="Last Name">
+                  </div>
+  
+                  <div class="mb-3">
+  
+                      <label for="exampleInputEmail1" class="form-label text-center">Email</label>
+  
+                      <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="someone@example.com">
+  
+                  </div>
+  
+                  <div class="mb-3">
+  
+                      <label for="exampleInputPassword1" class="form-label">Role Type</label>
+                      <select class="form-select" id="category">
+                        <option selected disabled>Select role type</option>
+                        <option value="travel">Content Creator</option>
+                        <option value="food">Admin</option>
+                        <!-- Dodajte preostale opcije kategorija -->
+                    </select>
+  
+                  </div>
+
+                  <div class="mb-3">
+
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="********">
+
+                  </div>
+
+                  <div class="mb-3">
+
+                    <label for="exampleInputPassword1" class="form-label">Confirm Password</label>
+
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="********">
+
+                  </div>
+
+                  <button type="submit" class="btn btn-primary mt-4">Create</button>
+  
+              </form>
+  
+          </div>
+  
+      </div>
+  
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateUserPage"
+}
+</script>
+
+<style scoped>
+.custom-toggler .navbar-toggler-icon {
+    filter: invert(1);
+}
+
+.padding-left-right-125px {
+    padding-left: 125px;
+    padding-right: 125px;
+}
+
+.text-navbar {
+    color: #f7f7f7 !important;
+    font-size: 17px !important;
+    font-weight: 500 !important;
+}
+
+.text-color {
+    color: #f7f7f7;
+}
+
+.glass {
+    background: linear-gradient(135deg, rgba(255, 255, 255, 0.1), rgba(255, 255, 255, 0));
+    backdrop-filter: blur(10px);
+    -webkit-backdrop-filter: blur(10px);
+    border-radius: 5px;
+    border: 1px solid rgba(255, 255, 255, 0.18);
+    box-shadow: 0 8px 32px 0 rgba(0, 0, 0, 0.37);
+}
+
+.card-form {
+    padding-top: 55px;
+    padding-bottom: 55px;
+    padding-left: 40px;
+    padding-right: 40px;
+    color: #D6D6D6;
+    max-width: 500px;
+}
+
+.border-navbar {
+    border-bottom: 2px solid rgba(255, 255, 255, 0.18);
+}
+
+.custom-bar {
+    max-height: calc(100vh - 200px);
+    /* Podesite visinu prema potrebi */
+    overflow-y: auto;
+}
+
+/* Stilizacija scroll bara */
+
+.custom-bar::-webkit-scrollbar {
+    width: 8px;
+    /* Širina scroll bara */
+    border-radius: 4px;
+    /* Zaobljenost scroll bara */
+}
+
+.custom-bar::-webkit-scrollbar-track {
+    background-color: transparent;
+    /* Providna pozadina scroll bara */
+}
+
+.custom-bar::-webkit-scrollbar-thumb {
+    border-radius: 4px;
+    /* Zaobljenost thumb-a */
+    background-color: rgba(255, 255, 255, 0.18);
+    /* Boja thumb-a */
+}
+
+/* Stilizacija forma komentara */
+
+.card-form {
+    padding-top: 55px;
+    padding-bottom: 55px;
+    padding-left: 40px;
+    padding-right: 40px;
+    color: #D6D6D6;
+    max-width: 500px;
+}
+
+.custom-height-textarea {
+    min-height: 150px;
+}
+
+/* Stilizacija paginacije */
+
+.pagination.glass-pagination .page-link {
+    background-color: transparent;
+    color: #f7f7f7;
+    border: none;
+    box-shadow: none;
+}
+
+.pagination.glass-pagination .page-link:hover {
+    background-color: rgba(255, 255, 255, 0.1);
+    border-radius: 5px;
+}
+
+.pagination.glass-pagination .page-item.active .page-link {
+    background-color: #f7f7f7;
+    color: #000;
+    border-radius: 5px;
+}
+
+.custom-border {
+    border: 1.5px solid rgba(255, 255, 255, 0.18);
+}
+</style>
